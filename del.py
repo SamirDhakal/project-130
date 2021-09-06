844e3dc457f9c9f027ade3495653d775a11db50a
SamirDhakal <68449594+SamirDhakal@users.noreply.github.com>
2021-09-05 20:20:23 -0400

    Update del.py

--- del.py
+++ del.py
@@ -1,15 +1,26 @@
+from typing import final
 import pandas as pd
 import csv
 
 df = pd.read_csv("final.csv")
-print(df.shape)
+print(df.columns)
 
-del df["Luminosity"]
-del df["Star_name"]
-del df["Distances"]
-del df["Masss"]
-del df["Radiuss"]
-del df["Unnamed: 0"]
-del df["Unnamed: 6"]
+#del df["Luminosity"]
+#del df["Star_name"]
+#del df["Distances"]
+#del df["Masss"]
+#del df["Radiuss"]
+#del df["Unnamed: 0"]
+#del df["Unnamed: 6"]
 
-df.to_csv("main.csv")
+df.drop(["Unnamed: 0", "Unnamed: 6", "Star_name", "Distances", "Masss", "Radiuss", "Luminosity"], axis = 1, inplace = True)
+print(df)
+final_data = df.dropna()
+print(final_data)
+final_data.reset_index(drop = True, inplace = True)
+print(final_data)
+final_data.to_csv("final_data.csv")
+final_data.info()
+final_data.describe()
+final_data.head(5)
+final_data.dtypes
